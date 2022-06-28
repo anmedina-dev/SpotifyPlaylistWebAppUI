6b780c1a91d1c0219edc15b03f0b9f662ffdfbff
anmedina-dev <antranikmedina@gmail.com>
2022-06-27 23:46:58 -0400

    Minor Landing page updates

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -9,6 +9,7 @@ export default function LandingPage(props) {
     <div className="landing-page-body">
       <div className="landing-page-header-section">
         <h2>Welcome {user.display_name}!</h2>
+        <h4>Learn about SPOTIFY PLAYLIST DROP &amp; PLAY </h4>
       </div>
     </div>
   );
